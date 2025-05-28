cbfe3dcdff45818447a2a5da4828484e1967e1a9
L-Otoo <144199475+Louisa-Otoo@users.noreply.github.com>
2025-05-28 08:49:26 +0000

    changed db.json to posts.json

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const jsonServer = require('json-server');
 const path = require('path');
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'db.json'));
+// const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const router = jsonServer.router(path.join(__dirname, 'data', 'posts.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
@@ -11,3 +12,5 @@ const port = process.env.PORT || 4000;
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
+
+
